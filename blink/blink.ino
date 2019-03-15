1c1367fdf6f945423d775529b6988cc90ee38ac0
Nilay Savant <nilayofficial@gmail.com>
2019-03-15 22:23:09 +0530

    Minor edits, to be pushed to github

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,25 +1,13 @@
 #include <Arduino.h>
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  Modified by Roger Clark. www.rogerclark.net for Maple mini 25th April 2015 , where the LED is on PB1
-  
+  STM32 - BluePill (stm32f103c8t6)
+  Example Sketch
+  Author: Nilay savant
  */
 
 // the setup function runs once when you press reset or power the board
 byte count = 0;
-char mess[1024]= {0};
+char mess[1024] = {0};
 void setup()
 {
   // initialize digital pin PB1 as an output.
@@ -32,8 +20,8 @@ void loop()
   snprintf(mess, 1024, "count executed");
   digitalWrite(PC13, HIGH); // turn the LED on (HIGH is the voltage level)
   snprintf(mess, 1024, "high executed");
-  delay(1000);               // wait for a second
-  digitalWrite(PC13, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);             // wait for a second
+  digitalWrite(PC13, LOW); // turn the LED off by making the voltage LOW
   snprintf(mess, 1024, "low executed");
-  delay(1000);               // wait for a second
+  delay(1000); // wait for a second
 }
